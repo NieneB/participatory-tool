598308fc7ad6282653f1627baee5099c918b028d
NieneB <niene.boeijen@gmail.com>
2024-12-11 15:22:34 +0100

    changing router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import Home from "./pages";
 import GraphView from "./pages/graph-view";
 import Intro from "./pages/explenation-view";
 
 const App = () => {
+
+
   return (
     <>
-      <BrowserRouter>
+      <HashRouter > 
         <Routes>
-          <Route path="/" index element={<Home />} />
+          <Route exact path="/" index element={<Home />} />
           <Route path="info" element={<Intro />} />
           <Route path="graph" element={<GraphView />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 };
