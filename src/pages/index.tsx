16513905690f381267c83620f5e4c2deb213810f
NieneB <niene.boeijen@gmail.com>
2025-02-20 11:11:50 +0100

    first texts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,15 +7,21 @@ import {
 const Home = () => {
   return (
     <>
-      <Styled.BackGround/>
+      <Styled.BackGround />
       <Styled.Main>
         <h1>Participatory approach to a complex spatially integrated design process
         </h1>
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-          sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud
-          exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat.:</p>
+        <h1>Welcome</h1>
+        <p> Welcome to the website for the participatory part of GO CAWH. GO CAWH is a Dutch initiative, led by Rijkswaterstaat. The goal of GO CAWH is to connect the institutional (led by Rijks Universiteit Groningen), community (led by Coöperatie Kloostersland) and participatory position (led by Design Academy Eindhoven) in water focussed innovation projects.
+
+
+          This website is dedicated to the approach from the participatory perspective.
+
+
+          On this website you will find a clear explanation of the scope of the process, led by the participatory process, the differences and connections between the participatory, institutional and community position and the tools to use in action.
+
+
+        </p>
         <div>
           <p>This tool is develop for: Design Academy Eindhoven</p>
           <p>This tool is develop by: Anne Vader, Niene Boeijen & Naomi Bueno de Mesquita</p>
--- src/pages/index_2.tsx
+++ src/pages/index_2.tsx
@@ -9,27 +9,24 @@ const Home2 = () => {
         sectetur adipiscing elit, sed do
         eiusmod tempor.
       </h1>
-      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Ut enim ad minim veniam, quis nostrud
-        exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat.:</p>
+      <p> To create an effective approach, it is very important to acknowledge the strengths and biases of each perspective. By respecting eachothers expertise, we can create a balanced situation in which our methods complement each other. Being transparant about the differences in positions helps to clarify the need for participation and to determine where a participatory approach would be valuable or not. Design Academy Eindhoven is approaching this through a cartographic lens: the map als a design method can help to visualize the complex relation between actors and their interests. It therefore creates a wider view on the situation at hand and helps to open up the possibilities in participation. </p>
+      <p>We’ve organized our approach in these three chapters: </p>
       <Styled.Buttons>
         <Link to="/scope">
           {" "}
           <Panel color='yellow' title='Scope' solid='not'>
-            <p>Lorem ipsum dolor sit amet,consectetur adipiscing elit</p>
+            <p>What are the steps, conditions and people involved in the participatory process?</p>
           </Panel>
         </Link>
         <Link to="/positions">
         <Panel color='purple' title='Positions' solid='not' >
-            <p>Lorem ipsum dolor sit amet,consectetur adipiscing elit</p>
+            <p>Why, what and who are the three positions involved in the water focussed innovation projects? </p>
           </Panel>
          
         </Link>
         <Link to="/actions">
         <Panel color='pink' title='Actions' solid='not' >
-            <p>Lorem ipsum dolor sit amet,consectetur adipiscing elit</p>
+            <p>How can you act on the opportunities and questions that arise? </p>
           </Panel>
         </Link>
       </Styled.Buttons>
