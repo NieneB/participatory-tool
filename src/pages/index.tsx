4ded714d719af6ca1e24043e7721a88f1ef7e501
NieneB <niene.boeijen@gmail.com>
2025-02-20 11:34:30 +0100

    base url

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,14 +13,12 @@ const Home = () => {
         </h1>
         <h1>Welcome</h1>
         <p> Welcome to the website for the participatory part of GO CAWH. GO CAWH is a Dutch initiative, led by Rijkswaterstaat. The goal of GO CAWH is to connect the institutional (led by Rijks Universiteit Groningen), community (led by Coöperatie Kloostersland) and participatory position (led by Design Academy Eindhoven) in water focussed innovation projects.
-
-
+        </p>
+        <p>
           This website is dedicated to the approach from the participatory perspective.
-
-
+        </p>
+        <p>
           On this website you will find a clear explanation of the scope of the process, led by the participatory process, the differences and connections between the participatory, institutional and community position and the tools to use in action.
-
-
         </p>
         <div>
           <p>This tool is develop for: Design Academy Eindhoven</p>
